7d01b964ac122744d587518bdbb9440912146849
Zach Hall <zachvac@gmail.com>
2024-11-16 12:17:53 -0500

    dummy commit

--- deploy.sh
+++ deploy.sh
@@ -4,3 +4,4 @@ aws s3 cp tab.js s3://www.youjustgotslapped.com/tab.js
 gsutil cp slaps.json gs://zach-slaps/slaps.json
 gcloud functions deploy function-2 --gen2 --region=us-east4 --runtime=python312 --source=slaps_function --entry-point=hello_http --trigger-http
 gcloud functions deploy function-1 --gen2 --region=us-east4 --runtime=python312 --source=stats_function --entry-point=hello_http --trigger-http
+echo "hello"
