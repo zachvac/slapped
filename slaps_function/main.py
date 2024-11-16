e06cdf34c5e6a81266cb1f9f00d5a2e2eb042c2e
Zach Hall <zachvac@gmail.com>
2024-11-16 16:05:50 -0500

    added bullshit

--- slaps_function/main.py
+++ slaps_function/main.py
@@ -21,3 +21,5 @@ def hello_http(request):
     bucket = client.get_bucket('zach-slaps')
     blob = bucket.get_blob('slaps.json')
     return(blob.download_as_text(), 200, headers)
+
+random bullshit that won't compile
