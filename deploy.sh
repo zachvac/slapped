0314529ca2371a8ebe51ebc9195f4bbc964d1491
Zach Hall <zachvac@gmail.com>
2024-11-16 13:43:49 -0500

    add deploy action

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 aws s3 cp --region us-east-1 index.html s3://www.youjustgotslapped.com/index.html
-aws s3 cp style.css s3://www.youjustgotslapped.com/style.css
-aws s3 cp tab.js s3://www.youjustgotslapped.com/tab.js
+aws s3 cp --region us-east-1 style.css s3://www.youjustgotslapped.com/style.css
+aws s3 cp --region us-east-1 tab.js s3://www.youjustgotslapped.com/tab.js
 gcloud storage cp slaps.json gs://zach-slaps/slaps.json # once we can add slaps on UI this can be removed
 gcloud functions deploy function-2 --gen2 --region=us-east4 --runtime=python312 --source=slaps_function --entry-point=hello_http --trigger-http
 gcloud functions deploy function-1 --gen2 --region=us-east4 --runtime=python312 --source=stats_function --entry-point=hello_http --trigger-http
